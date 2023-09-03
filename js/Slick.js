7849f9914f9baa905e22652ee4a320768cb2b89a
한지희, Jihee Han <hatrix1014@gmail.com>
2023-09-03 22:20:59 +0900

    Updated 0903

---
+++ js/Slick.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.editor_choice .swiper_section .swiper_wrapper').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+})
