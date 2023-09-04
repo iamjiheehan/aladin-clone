f5f56848915c651d00d4aa0df884dee3839541b3
iamjiheehan <hatrix1014@gmail.com>
2023-09-04 10:12:43 +0900

    Updated Slick

--- js/Slick.js
+++ js/Slick.js
@@ -1,8 +1,19 @@
 $(function () {
-    $('.editor_choice .swiper_section .swiper_wrapper').slick({
+    $(".editor_choice .swiper_section .swiper_wrapper").slick({
         slidesToShow: 4,
         slidesToScroll: 4,
-        autoplay: true,
+        // autoplay: true,
         autoplaySpeed: 3000,
+        pauseOnHover: true,
+        // arrows: true,
     });
-})
+    $(".editor_btn .editor-button-prev").on('click',function(e) {
+        //e.preventDefault();
+        $(".editor_choice .swiper_section .swiper_wrapper").slick("slickPrev");
+    });
+
+    $(".editor_btn .editor-button-next").on('click',function(e) {
+        //e.preventDefault();
+        $(".editor_choice .swiper_section .swiper_wrapper").slick("slickNext");
+    });
+});
