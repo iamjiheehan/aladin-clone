888dbd0fa34ead4ab65e682c0e6fef0b48d31950
iamjiheehan <hatrix1014@gmail.com>
2023-09-06 12:06:14 +0900

    Updated hover effect

---
+++ js/BookRotation.js
@@ -0,0 +1,28 @@
+$(function () {
+    var isRotated = false;
+
+    $(".book").click(function () {
+        if (isRotated) {
+            $(this).css("transform", "rotateY(35deg)");
+            isRotated = false;
+        } else {
+            $(this).css("transform", "rotateY(180deg)");
+            isRotated = true;
+        }
+    });
+
+    $(".book").hover(
+        function () {
+            if (!isRotated) {
+                $(this).css("transform", "rotateY(35deg)");
+                $("#divFlipImg .coverItem").show();
+            }
+        },
+        function () {
+            if (!isRotated) {
+                $(this).css("transform", "rotateY(0deg)");
+            }
+        }
+    );
+});
+
