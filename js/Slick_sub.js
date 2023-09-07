6bbff083f26ef42e436891f14cded15aaa5fad88
iamjiheehan <hatrix1014@gmail.com>
2023-09-07 12:02:03 +0900

    UPdate slick dots custom

---
+++ js/Slick_sub.js
@@ -0,0 +1,9 @@
+$(function () {
+    $("#sub_event .swiper_wrapper").slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        infinite: true,
+        dotsClass: 'dots_custom'
+    });
+});
