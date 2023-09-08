f2a7e56247a7ef3c742bef292db1f1d06bde7e66
iamjiheehan <hatrix1014@gmail.com>
2023-09-08 09:58:36 +0900

    Updated for right floating banner

--- js/floatingBn.js
+++ js/floatingBn.js
@@ -3,3 +3,11 @@ $(function (){
         $('html, body').animate({scrollTop: '0'}, 680);
     });
 });
+
+
+
+$(function (){
+    $(".goodsbn_wrap .goodsbn_box .goodsbn_close").on('click', function() {
+        $('.goodsbn_wrap').hide();
+    });
+});
