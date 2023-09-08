1caeaad36b2f2ba5868255c7efe785b9c4342fc7
iamjiheehan <hatrix1014@gmail.com>
2023-09-08 10:42:18 +0900

    Update for 서브페이지 100자평 카테고리 활성화

---
+++ js/LayerClick.js
@@ -0,0 +1,7 @@
+// 서브페이지 100자평 카테고리 활성화
+
+$(function () {
+    $(".re_select-icon").on("click",function () {
+        $(".layer_list").toggleClass("hidden");
+    });
+});
--- js/floatingBn.js
+++ js/floatingBn.js
@@ -1,3 +1,4 @@
+// 스크롤 업 버튼
 $(function (){
     $('.btnScrollToTop').on('click', function() {
         $('html, body').animate({scrollTop: '0'}, 680);
@@ -5,9 +6,10 @@ $(function (){
 });
 
 
-
+// 서브페이지 우측 상단 스크롤업 버튼
 $(function (){
     $(".goodsbn_wrap .goodsbn_box .goodsbn_close").on('click', function() {
         $('.goodsbn_wrap').hide();
     });
 });
+
