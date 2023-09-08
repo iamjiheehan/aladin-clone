9a16303077b0ea05f3bb1347caa5338824738a08
iamjiheehan <hatrix1014@gmail.com>
2023-09-08 17:58:53 +0900

    Updated 0908

--- js/LayerHover.js
+++ js/LayerHover.js
@@ -8,3 +8,10 @@ $(function () {
     });
 })
 
+//-----------------------------서브페이지 오늘 본 도서목록 리스트 //호버시 delete 버튼 활성화
+
+$(function () {
+    $(".recentList_item").on("hover", function(){
+
+    })
+});
