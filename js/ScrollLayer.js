a390f49438c09e95c8b03fa6756bf587fc003058
iamjiheehan <hatrix1014@gmail.com>
2023-09-11 17:59:25 +0900

    Update on process

--- js/ScrollLayer.js
+++ js/ScrollLayer.js
@@ -7,7 +7,7 @@ $(function () {
                     $("#fixed_layer_point").prop("clientHeight"))) *
                 100
         );
-        console.log(target);
+        console.log(target,scrollHeight);
         if (scroll == target) {
             $(".fixed_top-wrap").css("translate", "0");
             alert("도착했습니다.");
