6c399f63b487994d8378ea291ddefd7eb030a25c
iamjiheehan <hatrix1014@gmail.com>
2023-09-11 17:54:46 +0900

    Update on process

--- js/ScrollLayer.js
+++ js/ScrollLayer.js
@@ -1,10 +1,15 @@
 $(function () {
-    $(window).scroll(function(){
-        var target = $("#fixed_layer_point").top;
-        var x = $(document).scrollTop();
-
-        if(target == x){
-            $(".fixed_top-wrap").css("translate","0");
+    $(window).scroll(function () {
+        var scroll = $(document).scrollTop();
+        var target = Math.floor(
+            ($("#fixed_layer_point").scrollTop() /
+                ($("#fixed_layer_point").prop("scrollHeight") -
+                    $("#fixed_layer_point").prop("clientHeight"))) *
+                100
+        );
+        console.log(target);
+        if (scroll == target) {
+            $(".fixed_top-wrap").css("translate", "0");
             alert("도착했습니다.");
             console.log("Loading");
         }
