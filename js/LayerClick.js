8743341b4db7403cc48ade987c46601422575783
iamjiheehan <hatrix1014@gmail.com>
2023-09-12 17:55:41 +0900

    updated for foreign book section

--- js/LayerClick.js
+++ js/LayerClick.js
@@ -5,3 +5,14 @@ $(function () {
         $(".layer_list").toggleClass("hidden");
     });
 });
+
+
+//이책의 원서 번역서 클릭 레이어 이벤트
+
+$(function () {
+    $("#check_emtpy").on("click",function () {
+        $("#foreignBookResult").toggleClass("hidden");
+        $("#check_active").toggleClass("hidden");    
+        $("#check_emtpy").toggleClass("hidden");    
+    });
+});
