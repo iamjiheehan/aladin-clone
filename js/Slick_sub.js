0b405cd885f7b3ec9705e752c569ed718aac17c3
iamjiheehan <hatrix1014@gmail.com>
2023-09-12 12:15:07 +0900

    Update on process for bookSlick

--- js/Slick_sub.js
+++ js/Slick_sub.js
@@ -8,3 +8,24 @@ $(function () {
     });
 });
 
+// book flip
+
+$(function () {
+    $(".Bcoverwrap .cover .arr2_right").on("click", function (){
+            $(".Bcoverwrap .cover #divFlipImg").css("opacity","0");
+            $(".Bcoverwrap .cover .swiper_section").css("opacity","1");
+            $(".Bcoverwrap .cover .swiper_section .swiper_wrapper").slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+                infinite: true,
+            });
+        }
+    );
+    $(".Bcoverwrap .cover .arr2_right").on("mouseleave", function (){
+        $(".Bcoverwrap .cover #divFlipImg").css("opacity","1");
+        $(".Bcoverwrap .cover .swiper_section").css("opacity","0");
+    });
+
+    
+})
