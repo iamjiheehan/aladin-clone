18640421f09896bcaad311d2b383671e3682fa53
iamjiheehan <hatrix1014@gmail.com>
2023-09-12 14:49:28 +0900

    Updataed Flipbook slide show

--- js/Slick_sub.js
+++ js/Slick_sub.js
@@ -11,21 +11,35 @@ $(function () {
 // book flip
 
 $(function () {
-    $(".Bcoverwrap .cover .arr2_right").on("click", function (){
-            $(".Bcoverwrap .cover #divFlipImg").css("opacity","0");
-            $(".Bcoverwrap .cover .swiper_section").css("opacity","1");
-            $(".Bcoverwrap .cover .swiper_section .swiper_wrapper").slick({
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots: true,
-                infinite: true,
-            });
+    var $index = 0; 
+    var item = $(".Bcoverwrap .cover .swiper_section .swiper_wrapper .swiper_item");
+    var max = item.length;
+
+    $(".Bcoverwrap .cover .arr2_right").on("click", function () {
+        if ($index < max - 1) { 
+            $index++;
+            console.log("클릭후 인덱스 값은", $index);
+            $(".Bcoverwrap .cover #divFlipImg").css("opacity", "0");
+            $(".Bcoverwrap .cover .swiper_section").css("opacity", "1");
+            var translateXValue = $index * -502 + "px";
+            $(".Bcoverwrap .cover .swiper_section .swiper_wrapper").css("transform", "translateX(" + translateXValue + ")");
         }
-    );
-    $(".Bcoverwrap .cover .arr2_right").on("mouseleave", function (){
-        $(".Bcoverwrap .cover #divFlipImg").css("opacity","1");
-        $(".Bcoverwrap .cover .swiper_section").css("opacity","0");
+        else if ($index == max - 1) {
+            $("#lastpic").css("opacity", "1");
+            // Reset the opacity after 2 seconds
+            setTimeout(function () {
+                $("#lastpic").css("opacity", "0");
+            }, 2000); // 2000 milliseconds = 2 seconds
+        }
     });
 
-    
-})
+    $(".Bcoverwrap .cover .arr2_left").on("click", function () {
+        if ($index > 0) {
+            $index--;
+            console.log("클릭후 인덱스 값은", $index);
+            var translateXValue = $index * -502 + "px";
+            $(".Bcoverwrap .cover .swiper_section .swiper_wrapper").css("transform", "translateX(" + translateXValue + ")");
+        }
+    });
+});
+
