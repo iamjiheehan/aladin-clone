47358d24380fbfa68f377268363f3aca35c56a57
iamjiheehan <hatrix1014@gmail.com>
2023-09-12 09:42:55 +0900

    Updated Jquery for scroll event

--- js/ScrollLayer.js
+++ js/ScrollLayer.js
@@ -1,17 +1,24 @@
+// $(function () {
+//     $(document).scroll(function() {
+//         var currentScrollValue =  $(window).scrollTop()
+//         if (currentScrollValue > 1200) {
+//             $(".fixed_top-wrap").css('transform', 'translate(0, 0)');
+//         } else {
+//             $(".fixed_top-wrap").css('transform', 'translate(0, -150px)');
+//         }
+//     });
+// });
+
+
 $(function () {
-    $(window).scroll(function () {
-        var scroll = $(document).scrollTop();
-        var target = Math.floor(
-            ($("#fixed_layer_point").scrollTop() /
-                ($("#fixed_layer_point").prop("scrollHeight") -
-                    $("#fixed_layer_point").prop("clientHeight"))) *
-                100
-        );
-        console.log(target,scrollHeight);
-        if (scroll == target) {
-            $(".fixed_top-wrap").css("translate", "0");
-            alert("도착했습니다.");
-            console.log("Loading");
+    $(document).scroll(function() {
+        var currentScrollValue = $(document).scrollTop();
+        var fixedLayerPointTop = $("#fixed_layer_point").offset().top;
+
+        if (currentScrollValue >= fixedLayerPointTop) {
+            $(".fixed_top-wrap").css('transform', 'translate(0, 0)');
+        } else {
+            $(".fixed_top-wrap").css('transform', 'translate(0, -150px)');
         }
     });
 });
