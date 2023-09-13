0c3635d554a7ab1606811a01f5a4f05505a4ec66
iamjiheehan <hatrix1014@gmail.com>
2023-09-13 14:59:48 +0900

    Updated for margin-right error

--- js/Slick.js
+++ js/Slick.js
@@ -69,23 +69,27 @@ $(function () {
 
 
 $(function () {
-    $(".editor_choice #editorchoice .swiper_wrapper").slick({
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        // autoplay: true,
-        // autoplaySpeed: 3000,
-        // pauseOnHover: true,
-        // arrows: true,
-    });
-    $(".editor_btn .editor-button-prev").on('click',function(e) {
-        //e.preventDefault();
-        $(".editor_choice #editorchoice .swiper_wrapper").slick("slickPrev");
+    $("#editorchoice_tab").on('hover',function(){
+
+        $(".editor_choice #editorchoice .swiper_wrapper").slick({
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            // autoplay: true,
+            // autoplaySpeed: 3000,
+            // pauseOnHover: true,
+            // arrows: true,
+        });
+        $(".editor_btn .editor-button-prev").on('click',function(e) {
+            //e.preventDefault();
+            $(".editor_choice #editorchoice .swiper_wrapper").slick("slickPrev");
+        });
+    
+        $(".editor_btn .editor-button-next").on('click',function(e) {
+            //e.preventDefault();
+            $(".editor_choice #editorchoice .swiper_wrapper").slick("slickNext");
+        });
     });
 
-    $(".editor_btn .editor-button-next").on('click',function(e) {
-        //e.preventDefault();
-        $(".editor_choice #editorchoice .swiper_wrapper").slick("slickNext");
-    });
 });
 
 
