25a346805f3d042fe7beb279b827aa227b0ba56c
iamjiheehan <hatrix1014@gmail.com>
2023-09-13 10:54:09 +0900

    Updated hover event

--- js/LayerHover.js
+++ js/LayerHover.js
@@ -11,9 +11,23 @@ $(function () {
 //-----------------------------서브페이지 오늘 본 도서목록 리스트 //호버시 delete 버튼 활성화
 
 $(document).ready(function() {
-    $(".fiximg1").hover(function() {
-        $(this).closest(".fix_del").removeClass("hidden");
+    // Handle mouse enter for showing .fix_del
+    $(document).on('mouseenter', '#recentList li', function() {
+        $(this).find(".fix_del").removeClass("hidden");
+    });
+
+    // Handle mouse leave for hiding .fix_del
+    $(document).on('mouseleave', '#recentList li', function() {
+        $(this).find(".fix_del").addClass("hidden");
+    });
+
+    // Handle click on .fix_del to hide the list item
+    $(document).on('click', '.fix_del', function() {
+        $(this).closest("li").hide();
     });
 });
+
+
+
 
 
