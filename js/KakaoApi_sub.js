1a5a58eca0d882ff0cb88b8d54ef2924ac89ea4b
iamjiheehan <hatrix1014@gmail.com>
2023-09-13 10:41:39 +0900

    Updated for click and buy section in subpage

--- js/KakaoApi_sub.js
+++ js/KakaoApi_sub.js
@@ -1,34 +1,26 @@
-// ----------------------------------------------서브페이지 마지막 섹션 API 호출 및 Slick 적용 
+// ----------------------------------------------관련 구매상품 섹션 API 호출 및 Slick 적용 
 
 $(document).ready(function() {
 
     $(function () {
         var bookTitles = [
-            "88번 버스의 기적",
-            "타라바스",
-            "지식의 기초",
-            "전쟁과 죄책",
-            "죽은자의 집 청소",
-            "재일코리안 스포츠 영웅 열전",
-            "아주 희미한 빛으로도",
-            "연남동 빙굴빙굴 빨래방",
-            "메리골드 마음 세탁소",
-            "또 못 버린 물건들",
-            "너무나 많은 여름이",
-            "있을 법한 모든것",
-            "순도 100퍼센트의 휴식",
-            "오펜하이머 각본집",
-            "탱크",
-            "아메리칸 프로메테우스",
-            "ADHD 우울증 치매 이렇게 고쳐라",
-            "엄마의 10억",
-            "벨기에 에세이",
-            "감염 동물",
-            "완벽이 온다",
-            "물리적 힘"
+            "원씽 The One Thing",
+            "유연함의 힘",
+            "회복탄력성",
+            "집착의 법칙",
+            "초집중",
+            "기브앤테이크",
+            "세이노의 가르침",
+            "네 안에 잠든 거인을 깨워라",
+            "밥 프록터 부의 원리",
+            "최적의 공부 뇌",
+            "비욘드 디스럽션, 파괴적 혁신을 넘어",
+            "미래의 나를 구하러 갑니다.",
+            "일의 철학",
+            "그레이트 마인드셋"
         ];
     
-        var $swiperWrapper = $('#last_bookwrap .swiper_section .swiper_wrapper');
+        var $swiperWrapper = $('.last_wrap-buy #last_bookwrap .swiper_section .swiper_wrapper');
     
         bookTitles.forEach(function (title) {
             $.ajax({
@@ -57,7 +49,7 @@ $(document).ready(function() {
                 }
     
                 $(function createSlick() {
-                    $("#last_bookwrap .swiper_section .swiper_wrapper").not('.slick-initialized').slick({
+                    $(".last_wrap-buy #last_bookwrap .swiper_section .swiper_wrapper").not('.slick-initialized').slick({
                         lazyLoad:'ondemand',
                         slidesToShow: 5,
                         slidesToScroll: 5,
@@ -65,14 +57,14 @@ $(document).ready(function() {
                         infinite: true,
                         dotsClass: 'dots_custom',
                     });
-                    $("#last_bookwrap .white_circle_prev").on('click',function(e) {
+                    $(".last_wrap-buy #last_bookwrap .white_circle_prev").on('click',function(e) {
                         // e.preventDefault();
-                        $("#last_bookwrap .swiper_section .swiper_wrapper").slick("slickPrev");
+                        $(".last_wrap-buy #last_bookwrap .swiper_section .swiper_wrapper").slick("slickPrev");
                     });
                 
-                    $("#last_bookwrap .white_circle_next").on('click',function(e) {
+                    $(".last_wrap-buy #last_bookwrap .white_circle_next").on('click',function(e) {
                         // e.preventDefault();
-                        $("#last_bookwrap .swiper_section .swiper_wrapper").slick("slickNext");
+                        $(".last_wrap-buy #last_bookwrap .swiper_section .swiper_wrapper").slick("slickNext");
                     });
     
                     $(window).on( 'resize', createSlick );
@@ -82,45 +74,124 @@ $(document).ready(function() {
     });
 });
 
-//------------------오늘 본 상품----------------------------
 
-$(function() {
-    var bookTitles = [
-        "퓨처 셀프",
-        "세이노의 가르침",
-        "슈퍼노멀",
-        "역행자",
-        "집착의 법칙",
-        "모든 멋진 일에는 두려움이 따른다",
-        "혼자 있는 새벽 4시의 힘",
-        "파리의 심리학 카페",
-        "멘탈을 회복하는 연습",
-    ];
+// ----------------------------------------------서브페이지 관련 클릭상품 섹션 API 호출 및 Slick 적용 
 
-    var $recentList = $('#recentList');
+$(document).ready(function() {
 
-    bookTitles.forEach(function (title) {
-        $.ajax({
-            method: "GET",
-            url: "https://dapi.kakao.com/v3/search/book?target=title",
-            data: $.param({ query: title, size: 1 }), 
-            headers: { Authorization: "KakaoAK 6d7be6265b1495468abf689fe747c801" }
-        })
-        .done(function (msg) {
-            if (msg.documents.length > 0) {
-                var book = msg.documents[0]; 
+    $(function () {
+        var bookTitles = [
+            "아무도 가지 않은 길에 부가 있다",
+            "오리지널스",
+            "퓨처 셀프",
+            "집착의 법칙",
+            "슈퍼노멀",
+            "기브앤테이크",
+            "세이노의 가르침",
+            "네 안에 잠든 거인을 깨워라",
+            "밥 프록터 부의 원리",
+            "최적의 공부 뇌",
+            "비욘드 디스럽션, 파괴적 혁신을 넘어",
+            "미래의 나를 구하러 갑니다.",
+            "대화력의 비밀",
+            "심리 대화술"
+        ];
+    
+        var $swiperWrapper = $('.last_wrap-click #last_bookwrap .swiper_section .swiper_wrapper');
+    
+        bookTitles.forEach(function (title) {
+            $.ajax({
+                method: "GET",
+                url: "https://dapi.kakao.com/v3/search/book?target=title",
+                data: $.param({ query: title, size: 1 }), 
+                headers: { Authorization: "KakaoAK 6d7be6265b1495468abf689fe747c801" }
+            })
+            .done(function (msg) {
+                if (msg.documents.length > 0) {
+                    var book = msg.documents[0]; 
+    
+                    var $swiperItem = $("<div class='swiper_item'></div>");
+                    var $cover = $("<div class='cover'></div>");
+    
+                    $cover.append("<a href='#'><img src='" + book.thumbnail + "' alt='" + book.title + "' /></a>");
+    
+                    $swiperItem.append($cover);
+    
+                    var $text = $("<div class='text'></div>");
+                    var $title = $("<div class='title'></div>");
+                    $title.append("<a href='#'>" + book.title + "</a>");
+                    $text.append($title);
+                    $swiperItem.append($text);
+                    $swiperWrapper.append($swiperItem);
+                }
+    
+                $(function createSlick() {
+                    $(".last_wrap-click #last_bookwrap .swiper_section .swiper_wrapper").not('.slick-initialized').slick({
+                        lazyLoad:'ondemand',
+                        slidesToShow: 5,
+                        slidesToScroll: 5,
+                        dots: true,
+                        infinite: true,
+                        dotsClass: 'dots_custom',
+                    });
+                    $(".last_wrap-click #last_bookwrap .white_circle_prev").on('click',function(e) {
+                        // e.preventDefault();
+                        $(".last_wrap-click #last_bookwrap .swiper_section .swiper_wrapper").slick("slickPrev");
+                    });
                 
-                var $closeBtn = $("<div class='fix_del' style='display: none'><a href='javascript:void(0);' class='RecentDelete'><img src='./img/btn_del.png' alt='' border='0' /></a></div>");
-                var $Wrapper = $("<li></li>");
-                var $Item = $("<div class='recentList_item'></div>");
-
-                $Item.append("<a href='javascript:void(0);'><img border='0' class='fiximg1' src='" + book.thumbnail + "' alt='" + book.title + "' /></a>");
-                
-                $Wrapper.append($closeBtn);
-                $Wrapper.append($Item);
-                $recentList.append($Wrapper);
-            }
+                    $(".last_wrap-click #last_bookwrap .white_circle_next").on('click',function(e) {
+                        // e.preventDefault();
+                        $(".last_wrap-click #last_bookwrap .swiper_section .swiper_wrapper").slick("slickNext");
+                    });
+    
+                    $(window).on( 'resize', createSlick );
+                });
+            });
         });
     });
 });
 
+//----------------오늘 본 상품----------------------------
+
+$(document).ready(function() {
+
+    $(function() {
+        var bookTitles = [
+            "나는 어떻게 삶의 해답을 찾는가",
+            "슈퍼노멀",
+            "세이노의 가르침",
+            "퓨처 셀프",
+            "오리지널스",
+            "네 안에 잠든 거인을 깨워라",
+            "역행자",
+            "대화력의 비밀",
+            "아주 작은 습관의 힘",
+        ];
+
+        var $recentList = $('#recentList');
+
+        bookTitles.forEach(function (title) {
+            $.ajax({
+                method: "GET",
+                url: "https://dapi.kakao.com/v3/search/book?target=title",
+                data: $.param({ query: title, size: 1 }), 
+                headers: { Authorization: "KakaoAK 6d7be6265b1495468abf689fe747c801" }
+            })
+            .done(function (msg) {
+                if (msg.documents.length > 0) {
+                    var book = msg.documents[0]; 
+                    
+                    var $closeBtn = $("<div class='fix_del hidden'><a href='javascript:void(0);' class='RecentDelete'><img src='./img/btn_del.png' alt='' border='0' /></a></div>");
+                    var $Wrapper = $("<li></li>");
+                    var $Item = $("<div class='recentList_item'></div>");
+
+                    $Item.append("<a href='javascript:void(0);'><img border='0' class='fiximg1' src='" + book.thumbnail + "' alt='" + book.title + "' /></a>");
+                    
+                    $Wrapper.append($closeBtn);
+                    $Wrapper.append($Item);
+                    $recentList.append($Wrapper);
+                }
+            });
+        });
+    });
+});
